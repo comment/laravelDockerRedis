29f259fc00ecff76812271c28af81160e6620d3f
Sergey <obrazcovsergey@gmail.com>
2024-08-09 13:44:41 +0300

    gogogo

---
+++ app/Jobs/SendRegistrationEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendRegistrationEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public $email;
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $email = $this->email;
+        Mail::send([], [], function ($message) use ($email)
+        {
+            $message
+                ->to($email)
+                ->subject('Hello, world!')
+                ->setBody('Welcome to the application!');
+        });
+    }
+}
